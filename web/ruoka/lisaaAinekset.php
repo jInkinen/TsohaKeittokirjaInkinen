9c370640c0053c632c79f37cd9bfcc01cf260d3a
jInkinen <juha.inkinen@hotmail.com>
2012-09-27 17:01:47 +0300

    aines fixailuita

--- web/ruoka/lisaaAinekset.php
+++ web/ruoka/lisaaAinekset.php
@@ -3,11 +3,22 @@ $aines = $_POST["aines[]"];
 $maara = $_POST["maara[]"];
 $ruokaID = $_GET["id"];
 
+if (!isset($aines) || !isset($maara) || !isset($ruokaID)) {
+    die("Vaadittuja arvoja ei lähetetty sivulle. Tulitko sivulle oikeaa reittiä?");
+}
+
 include("../TKyhteys.php");
-$insert = $TKyhteys->prepare("INSERT INTO ruoanAinekset (nimi, maara, ruokaID) VALUES (?, ?, ?)");
+
 
 for ($i = 1; $i <= count($aines); $i++) {
-    $insert->execute(array($aines[$i], $maara[$i], $ruokaID));
+    $insert = $TKyhteys->prepare("INSERT INTO aines (nimi) VALUES (" . $aines . ")");
+    $insert->execute();
+    
+    $select = $TKyhteys->prepare("SELECT ID FROM aines WHERE nimi=" . $aines . ")");
+    $insert->execute();
+    
+    $insert2 = $TKyhteys->prepare("INSERT INTO ruoanainekset (RuokaID, AinesID, maara) VALUES (" . $ruokaID . ", " . null . "," . $maara . ")");
+    $insert2->execute();
 }
  
 ?>
